927c228d74676e271302087a1a8dff98bb2f3d23
Patrick O'Neill <pgoneill@gmail.com>
2011-07-12 14:26:52 -0400

    Minor fixes to jQuery version.

--- jquery.resizeStop.js
+++ jquery.resizeStop.js
@@ -22,8 +22,10 @@
 (function ($, setTimeout) {
 
     var $window = $(window),
+        cache = $([]),
         last = 0,
-        timer = 0;
+        timer = 0,
+        size = {};
 
     /**
         Handles window resize events.
@@ -46,29 +48,41 @@
         } else {
             clearTimeout(timer);
             timer = last = 0;
-            $window.trigger('resizestop');
+            size.width = $window.width();
+            size.height = $window.height();
+            cache.trigger('resizestop');
         }
     }
 
     $.resizestop = {
+        propagate: false,
         threshold: 500
     };
 
     $.event.special.resizestop = {
         setup: function (data, namespaces) {
-            $window.bind('resize', onWindowResize);
+            cache = cache.not(this); // Prevent duplicates.
+            cache = cache.add(this);
+            if (cache.length === 1) {
+                $window.bind('resize', onWindowResize);
+            }
         },
         teardown: function (namespaces) {
-            $window.unbind('resize', onWindowResize);
+            cache = cache.not(this);
+            if (!cache.length) {
+                $window.unbind('resize', onWindowResize);
+            }
         },
         add: function (handle) {
             var oldHandler = handle.handler;
             handle.handler = function (e) {
+                // Generally, we don't want this to propagate.
+                if (!$.resizestop.propagate) {
+                    e.stopPropagation();
+                }
                 e.data = e.data || {};
-                e.data.size = {
-                    width: $window.width(),
-                    height: $window.height()
-                };
+                e.data.size = e.data.size || {};
+                $.extend(e.data.size, size);
                 return oldHandler.apply(this, arguments);
             };
         }
